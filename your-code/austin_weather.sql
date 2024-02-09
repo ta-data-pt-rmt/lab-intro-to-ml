c780c3de396ce99f68ffa8adcdf09a607922e73d
whitz22 <whitmacs22@gmail.com>
2024-02-09 23:00:45 +0100

    *updated*

--- your-code/austin_weather.sql
+++ your-code/austin_weather.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE weather;
+USE weather;
 CREATE TABLE IF NOT EXISTS austin_weather (
     `Date` DATETIME,
     `TempHighF` INT,
@@ -1346,3 +1348,36 @@ INSERT INTO austin_weather VALUES
     ('2017-07-29 00:00:00',107,92,77,'72','64','55','82','51','19','29.91','29.86','29.79','10','10','10','12','4','17','0',' '),
     ('2017-07-30 00:00:00',106,93,79,'70','68','63','69','48','27','29.96','29.91','29.87','10','10','10','13','4','20','0',' '),
     ('2017-07-31 00:00:00',99,88,77,'66','61','54','64','43','22','30.04','29.97','29.91','10','10','10','12','4','20','0',' ');
+    
+    SHOW tables;
+	SELECT * FROM austin_weather;
+    DESCRIBE austin_weather;
+    SELECT * FROM austin_weather;
+    
+    -- Execersie
+    /*  How many days are recorded in the dataset?*/
+    SELECT COUNT(DISTINCT Date)
+	FROM austin_weather;
+
+/*  What is the day with the Highest Temperature in Fahrenheit (column TempHighF)*/
+SELECT MAX(TempHighF) FROM austin_weather;
+
+/* What is the average Humidity across all days? (column HumidityAvgPercent)*/
+SELECT * FROM austin_weather;
+SELECT AVG(HumidityAvgPercent) 
+FROM austin_weather;
+
+/*Top 10 days, where SeaLevelPressureAvgInches is the highest, knowing DewPointAvgF is higher than 28*/
+SELECT Date, SeaLevelPressureAvgInches
+FROM austin_weather
+WHERE DewPointAvgF > 28
+ORDER BY SeaLevelPressureAvgInches DESC
+LIMIT 10;
+
+
+
+  
+    
+
+    
+    
